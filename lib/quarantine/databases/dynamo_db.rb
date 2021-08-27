e24b263929536e963beecb9fb1db555e67965338
Emilio <emilio2hd@gmail.com>
2021-08-27 10:51:45 +0100

    Writes items in batches of 25 items due to dynamodb batch_write_item limitation

--- lib/quarantine/databases/dynamo_db.rb
+++ lib/quarantine/databases/dynamo_db.rb
@@ -12,6 +12,8 @@ class Quarantine
     class DynamoDB < Base
       extend T::Sig
 
+      BATCH_WRITE_MAXIMUM_ITEMS = T.let(25, Integer)
+
       Attribute = T.type_alias { { attribute_name: String, attribute_type: String, key_type: String } }
 
       sig { returns(Aws::DynamoDB::Client) }
@@ -42,17 +44,19 @@ class Quarantine
         ).void
       end
       def write_items(table_name, items)
-        @dynamodb.batch_write_item(
-          request_items: {
-            table_name => items.map do |item|
-              {
-                put_request: {
-                  item: item
+        items.each_slice(BATCH_WRITE_MAXIMUM_ITEMS) do |batch_items|
+          @dynamodb.batch_write_item(
+            request_items: {
+              table_name => batch_items.map do |item|
+                {
+                  put_request: {
+                    item: item
+                  }
                 }
-              }
-            end
-          }
-        )
+              end
+            }
+          )
+        end
       rescue Aws::DynamoDB::Errors::ServiceError
         raise Quarantine::DatabaseError
       end
